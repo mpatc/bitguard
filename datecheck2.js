0fb7d57f1ae901a97a6276da789f49cfeadcea16
mpatc <matthewpclancy@gmail.com>
2016-01-12 22:50:17 +0900

    Update datecheck2.js

--- datecheck2.js
+++ datecheck2.js
@@ -1,48 +1,26 @@
-var a = Date.now();
-
-var b = (a/86400000);
-
-var dayC = Math.floor(b);
-
-console.log(dayC)
-
 var age = function(x,y,z) {
+  var dateRaw = Date.now();
+  var dateDay = (dateRaw/86400000);
+  var dateCurrent = Math.floor(dateDay);
+  var dateGiv = new Date(x,y,z);
+  var dateGivRaw = dateGiv.getTime();
+  var dateGivDay = dateGivRaw/86400000;
+  var dateGiven = Math.floor(dateGivDay);
+  var differenceDay = Math.abs(dateCurrent- dateGiven);
+  var month = Math.floor(differenceDay/30);
+  var day1 = differenceDay - (month*30);
+  var year = Math.floor(differenceDay/365);
+  var month2 = month - (year*12);
+  var day2 = differenceDay - (month2*30);
+  if (differenceDay < 31) {
+    console.log(differenceDay + " days");
+      } else if (month < 12) {
+        console.log(month2 + " months " + day1 + " days");
+          } else {
+        console.log(year + " years " + month2 + " months " + day2 + " days");
+      };
+};
+age(2005, 9, 4);
 
-var aN = new Date(x,y,z);
 
-var bN = aN.getTime();
 
-var cN = (bN/86400000);
-
-var dayN = Math.floor(cN);
-
-var dif = (Math.abs(dayC-dayN))
-
-var mon = Math.floor(dif/30);
-
-var day1 = (dif - (mon*30));
-
-var year = Math.floor(dif/365)
-
-var mon2 = mon - (year*12)
-
-var day2 = (dif - (mon*30));
-
-if (dif < 31) {
-
-console.log(dif + " days")
-
-} else if (mon < 12) {
-
-console.log(mon + " months " + day1 + " days");
-
-} else {
-
-console.log(year + " years " + mon2 + " months " + day2 + " days")
-
-}
-
-}
-
-age(2004, 9, 4);
-intercom
